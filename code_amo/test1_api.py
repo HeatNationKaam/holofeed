8cee7156706b0c9f7070c52999887743f3a8f9de
Amaury Francou <amaury.francou@gmail.com>
2021-06-29 17:57:47 +0200

    Màj du fichier test_api.py | Fonctionnalité de récupération des messages IG

--- code_amo/test1_api.py
+++ code_amo/test1_api.py
@@ -1,47 +1,69 @@
-import requests
+import requests as r
+import json
+
+
+_TOKEN = "EAAHnSrlmBj4BAAlzMjKpdifhDpWx4Q6SFywVfFW1VRvBihSmy3PuXffByAZCQ7KthP9 \
+    YnqPnS5pcOcw1XOjRWhCkfj22qK8np5RqFCqZCSpkKSomKYQot1pvHigfCwT5GT0HEWGPRIy3 \
+        siIPZCA6Tx6C6u6HHgZCvuMgYyR2txKW0itCatw4"
+        
+
+_PARAMS = dict()
+_PARAMS["base_url"] = "https://graph.facebook.com/"
+_PARAMS["api_version"] = "v11.0/"
+
+def get_user_page(token=_TOKEN,params=_PARAMS) :
+    request = params["base_url"] + params["api_version"]
+    request += "me/accounts?access_token="
+    request += token
+    response = r.get(request)
+    return(json.loads(response.text))
+
+_USER_PAGE = get_user_page()
+
+def get_page_accessToken(token=_TOKEN,params=_PARAMS, user_page = _USER_PAGE) :
+    request = _PARAMS["base_url"] 
+    page_id = user_page['data'][1]['id']
+    request += page_id + '?'
+    request += 'fields=access_token&access_token=' + token
+    response = r.get(request)
+    return(json.loads(response.text))
+
+_ACCESS_TOKEN = get_page_accessToken()['access_token']
+
+def get_inbox_convIds(token=_TOKEN,params=_PARAMS,page_id=_USER_PAGE['data'][1]['id'], \
+                         accessToken=_ACCESS_TOKEN) :
+    request = params["base_url"] + params["api_version"]
+    request += page_id + '/conversations?platform=instagram'
+    request += '&access_token=' + accessToken
+    response = r.get(request)
+    return(json.loads(response.text))
+
+_INBOX_CONV_IDS = get_inbox_convIds()
+
+
+def get_messages_ids(token=_TOKEN,params=_PARAMS,page_id=_USER_PAGE['data'][1]['id'], \
+                     accessToken=_ACCESS_TOKEN, inbox_conv_ids=_INBOX_CONV_IDS) :
+    request = params["base_url"] + params["api_version"]
+    thread_id = inbox_conv_ids['data'][0]['id']
+    request += thread_id + '?fields=messages'
+    request += '&access_token=' + accessToken
+    response = r.get(request)
+    return(json.loads(response.text))
+
+_MESSAGES_IDS = get_messages_ids()
+
+def get_message(token=_TOKEN,params=_PARAMS,page_id=_USER_PAGE['data'][1]['id'], \
+                     accessToken=_ACCESS_TOKEN, messages_ids=_MESSAGES_IDS) :
+    request = params["base_url"] + params["api_version"]
+    message_id = messages_ids['messages']['data'][0]['id']
+    request += message_id + '?fields=message'
+    request += '&access_token=' + accessToken
+    response = r.get(request)
+    return(json.loads(response.text))
+
+_MESSAGE = get_message()
+print(_MESSAGE);
 
 
 
-req = "https://graph.facebook.com/v11.0/oauth/access_token?  \
-    grant_type=fb_exchange_token&          \
-    client_id=535783100778046& \
-    client_secret=121f28a430e922b06a52c53eacf72b75& \
-    fb_exchange_token=EAAHnSrlmBj4BAECQcPPrewnbkZAmoq6PVRkRscIPhLZByJpZCeM1vrJWGQ4MrasAnjN4Ua4PksjpUCm9HIhJCDjiGsxvaAiiR7cWcQnXKSOyatJDxW0GHGDBrZBPxak9jYc8qYZCbJTYTKSIi4a8VHsY8sICO4WfMjGS6QBO8UVeHqNeQQvef7hRrs7nZAhTqiaekBif8c48H45o43feypAd7dEvxtFxowpWn9AMsVcMGMXf1HQMMc" 
 
-requests.get(req)
-
-token = "EAAHnSrlmBj4BAECQcPPrewnbkZAmoq6PVRkRscIPhLZByJpZCeM1vrJWGQ4MrasAnjN4Ua4PksjpUCm9HIhJCDjiGsxvaAiiR7cWcQnXKSOyatJDxW0GHGDBrZBPxak9jYc8qYZCbJTYTKSIi4a8VHsY8sICO4WfMjGS6QBO8UVeHqNeQQvef7hRrs7nZAhTqiaekBif8c48H45o43feypAd7dEvxtFxowpWn9AMsVcMGMXf1HQMMc"
-token2 = "EAAHnSrlmBj4BAFrV5gZA3GCQzmZArK1NfhJIjYH9Dxe28l67cUltNN49eHdo5NUGRN3ulrXEinwsmHeZBenbSa9PaXksLY0HnTBry9j2c7VENZCsDef8kSYfFkCjI1BRG8WxmxCGmlXgWGNk41jmZB6P2gmoDTGtjmF2MX2eCniKIDLPwb088uLyi9529XFgLRATIQ8yhJzgZBUZCAh14ahyYkJMBwM0tCwOBZBKmUh7ksrVho4xE9Bc"
-token2 = "EAAHnSrlmBj4BAE1bZCev2rYjwPjDKv6Lcl7E45WDzkqyKbtFDlnxQeU2d2P6AbEFsNSywpF23iywhN9Imxt55PvqIOHNTZADGzZBUL1BqxIetsiutUcZCe3G1AKJTVEDGKBIFAGFCznoZBZBW2ZBGVvXBmTJuU5rx0KaynPztJ8xao4oAS6Y33tDBKBh2H61zbGHJac23GHyIoIYw3fdljLIhFVvKpvD7IQSseGjZBLCJXW7hpVi9OEk"
-
-
-req2 = "https://graph.facebook.com/v11.0/me/accounts?access_token=" + token2
-
-
-print(requests.get(req2).text)
-
-
-req3 =  "https://graph.facebook.com/v11.0/107090291604237?fields=instagram_business_account&access_token=" + token2
-print(requests.get(req3).text)
-
-IGID = '17841447991814103'
-
-
-req4 = "https://graph.facebook.com/v11.0/17841447991814103?fields=business_discovery.username(cristiano){followers_count,media_count}&access_token=" + token2
-    
-print(requests.get(req4).text)
-
-info = '{biography,id,followers_count,media_count,username,website}'
-
-req5 = "https://graph.facebook.com/v11.0/17841447991814103?fields=business_discovery.username(cristiano)" + info + "&access_token=" + token2
-print(requests.get(req5).text)
-
-import facebook
-
-graph = facebook.GraphAPI(access_token=token2, version="3.1")
-
-
-
-req = "https://graph.facebook.com/v9.0/me/accounts?access_token=" + token2
-
-requests.get(req).text
