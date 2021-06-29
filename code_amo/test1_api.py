92e9e8ed3326c4d32229919e320c42a2b8510cbf
Amaury Francou <amaury.francou@gmail.com>
2021-06-29 22:11:15 +0200

    Légères améliorations de test1_api.py

--- code_amo/test1_api.py
+++ code_amo/test1_api.py
@@ -56,14 +56,23 @@ def get_message(token=_TOKEN,params=_PARAMS,page_id=_USER_PAGE['data'][1]['id'],
                      accessToken=_ACCESS_TOKEN, messages_ids=_MESSAGES_IDS) :
     request = params["base_url"] + params["api_version"]
     message_id = messages_ids['messages']['data'][0]['id']
-    request += message_id + '?fields=message'
+    request += message_id + '?fields=story'
     request += '&access_token=' + accessToken
     response = r.get(request)
     return(json.loads(response.text))
 
 _MESSAGE = get_message()
-print(_MESSAGE);
+print(_MESSAGE)
 
+_IGSID = '6341037472603944'
 
+def get_user_infos(params=_PARAMS,igsid=_IGSID,accessToken=_ACCESS_TOKEN) :
+    request = params["base_url"] + params["api_version"]
+    request += igsid
+    request += '?fields=name,profile_pic&access_token=' + accessToken
+    response = r.get(request)
+    return(json.loads(response.text))
 
+#USER_INFO = get_user_infos()
+#print(USER_INFO)
 
